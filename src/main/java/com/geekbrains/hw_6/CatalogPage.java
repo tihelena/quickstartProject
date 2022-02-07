c80cfa52056dad51bf013b6ab6e29a7911d6f1b7
tihelena <elenatikhon@yandex.ru>
2022-02-07 19:43:06 +0300

    home_work6

---
+++ src/main/java/com/geekbrains/hw_6/CatalogPage.java
@@ -0,0 +1,47 @@
+package com.geekbrains.hw_6;
+
+import com.geekbrains.lesson6.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class CatalogPage extends BasePage{
+
+    public CatalogPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//div[@data-catalog='cat']")
+    public WebElement catalog;
+
+    public CatalogPage showTopList() {
+        catalog.click();
+        return this;
+    }
+
+    public final static String SELECT_ROSES_BY_XPATH = "//a[@href='/retail/catalogue/roses/index.html']//img[@src='/img/logos/_s50/rose.png']";
+    @FindBy(xpath = SELECT_ROSES_BY_XPATH )
+    public WebElement roses;
+
+    public CatalogPage selectFromTopList() {
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(SELECT_ROSES_BY_XPATH)));
+        roses.click();
+
+        return this;
+    }
+
+    public final static String SELECT_TYPE_OF_ROSES_BY_XPATH = "//a[@href='/retail/catalogue/roses/climbing.html']";
+    @FindBy(xpath = SELECT_TYPE_OF_ROSES_BY_XPATH)
+    public WebElement typeOfRoses;
+
+    public RosePage setTypeOfRoses() throws InterruptedException {
+        Thread.sleep(3000);
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(SELECT_TYPE_OF_ROSES_BY_XPATH)));
+        typeOfRoses.click();
+        return new RosePage(driver);
+
+
+    }
+}
---
+++ src/test/java/com/geekbrains/hw_6/FlowersPageObjectTest.java
@@ -0,0 +1,62 @@
+package com.geekbrains.hw_6;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.time.Duration;
+
+public class FlowersPageObjectTest {
+
+    WebDriver driver;
+    WebDriverWait webDriverWait;
+    Actions actions;
+    private final static String SHOP_URL = "https://podvorje.ru";
+
+    @BeforeAll
+    static void registerDriver() {
+        WebDriverManager.safaridriver().setup();
+    }
+
+    @BeforeEach
+    void initDriver() {
+        driver = new SafariDriver();
+        driver.get("https://podvorje.ru");
+        driver.manage().window().maximize();
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        actions = new Actions(driver);
+    }
+
+    @Test
+    void addRoseToBasket() throws InterruptedException {
+        new CatalogPage(driver)
+                .showTopList()
+                .selectFromTopList()
+                .setTypeOfRoses()
+                .choiceRose()
+                .bayRose()
+                .goToBasket()
+                .addOneMore()
+                .checkResult();
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+}
